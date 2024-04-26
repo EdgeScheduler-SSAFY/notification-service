7bc7c871417edca9498e77c57c51d4f21bd49911
오형택 <oh052679@gmail.com>
2024-04-26 16:23:39 +0900

    ✨Feat :  add Kafka sample code & docker-compose.yml & MongoDB datasource config
    Change-Id: I642e9febd63b5b9ae3dd5ed6909cb474263ed2f5

--- src/main/java/com/edgescheduler/notificationservice/controller/TestController.java
+++ src/main/java/com/edgescheduler/notificationservice/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.edgescheduler.notificationservice.controller;
 
+import com.edgescheduler.notificationservice.service.KafkaService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +9,14 @@ import reactor.core.publisher.Mono;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class TestController {
+
+    private final KafkaService kafkaService;
 
     @GetMapping("/test")
     public Mono<String> test(){
-        return Mono.just("test")
-                .doOnNext(log::info);
+        return Mono.just("test Kafka messaging")
+                .doOnNext(message -> kafkaService.publish("test", message));
     }
 }
---
+++ src/main/java/com/edgescheduler/notificationservice/service/KafkaService.java
@@ -0,0 +1,24 @@
+package com.edgescheduler.notificationservice.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KafkaService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void publish(String topic, String message) {
+        kafkaTemplate.send(topic, message);
+    }
+
+    @KafkaListener(topics = "test")
+    private void consume(String message) {
+        log.info("Consumed message: {}", message);
+    }
+}
