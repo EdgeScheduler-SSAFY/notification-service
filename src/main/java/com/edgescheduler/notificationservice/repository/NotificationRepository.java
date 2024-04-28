1f7315783748655cf8f3c8d202ebd9aa807522c6
오형택 <oh052679@gmail.com>
2024-04-29 00:12:10 +0900

    ✨Feat :  add Kafka config
    Change-Id: I3ef5060821f2e2b133a1de7027dffb060ead0024

--- src/main/java/com/edgescheduler/notificationservice/repository/NotificationRepository.java
+++ src/main/java/com/edgescheduler/notificationservice/repository/NotificationRepository.java
@@ -3,7 +3,6 @@ package com.edgescheduler.notificationservice.repository;
 import com.edgescheduler.notificationservice.domain.Notification;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-public interface NotificationRepository extends
-    ReactiveMongoRepository<Notification, String> {
+public interface NotificationRepository extends ReactiveMongoRepository<Notification, String> {
 
 }
--- src/main/java/com/edgescheduler/notificationservice/service/KafkaService.java
+++ src/main/java/com/edgescheduler/notificationservice/service/KafkaService.java
@@ -1,9 +1,12 @@
 package com.edgescheduler.notificationservice.service;
 
+import java.util.concurrent.atomic.AtomicLong;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.adapter.ConsumerRecordMetadata;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -11,14 +14,20 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class KafkaService {
 
+    private final EventSinkManager eventSinkManager;
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private final AtomicLong counter = new AtomicLong(0);
 
     public void publish(String topic, String message) {
         kafkaTemplate.send(topic, message);
     }
 
-    @KafkaListener(topics = "test")
-    private void consume(String message) {
-        log.info("Consumed message: {}", message);
+    @KafkaListener(topics = "${KAFKA_NOTIFICATION_TOPIC}", groupId = "${KAFKA_GROUP_ID}")
+    private void consume(
+        ConsumerRecord<String, String> message,
+        ConsumerRecordMetadata metadata
+    ) {
+        log.info("Consumed message: {} | From partition: {} | From offset: {}", message.value(), metadata.partition(), metadata.offset());
+
     }
 }
