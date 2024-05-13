342643e31052d7eebc421868ec55ef1326b517f6
오형택 <oh052679@gmail.com>
2024-05-13 15:07:46 +0900

    ✨Feat :  remove unused HashMap
    Change-Id: I2220b58b32b1a6e3b12a4b54e0c2076a90a3493c

--- src/main/java/com/edgescheduler/notificationservice/service/MemberInfoService.java
+++ src/main/java/com/edgescheduler/notificationservice/service/MemberInfoService.java
@@ -1,11 +1,8 @@
 package com.edgescheduler.notificationservice.service;
 
-import com.edgescheduler.notificationservice.domain.MemberInfo;
 import com.edgescheduler.notificationservice.repository.MemberInfoRepository;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -14,26 +11,11 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class MemberInfoService {
 
-    private final Map<Integer, ZoneId> memberTimezoneMap = new ConcurrentHashMap<>();
     private final MemberInfoRepository memberInfoRepository;
 
     public Mono<ZoneId> getZoneIdOfMember(Integer memberId) {
-        return Mono.justOrEmpty(memberTimezoneMap.get(memberId))
-            .switchIfEmpty(memberInfoRepository.findById(memberId)
-                .map(memberInfo -> {
-                    ZoneId zoneId = ZoneId.of(memberInfo.getZoneId());
-                    memberTimezoneMap.put(memberId, zoneId);
-                    return zoneId;
-                })
-                .switchIfEmpty(Mono.just(ZoneOffset.UTC)));
-    }
-
-    public Mono<Void> upsertMemberTimezone(Integer memberId, String timezone) {
-        return memberInfoRepository.save(MemberInfo.builder()
-                .memberId(memberId)
-                .zoneId(timezone)
-                .build())
-            .doOnSuccess(memberInfo -> memberTimezoneMap.put(memberId, ZoneId.of(timezone)))
-            .then();
+        return memberInfoRepository.findById(memberId)
+            .map(memberInfo -> ZoneId.of(memberInfo.getZoneId()))
+            .switchIfEmpty(Mono.just(ZoneOffset.UTC));
     }
 }
