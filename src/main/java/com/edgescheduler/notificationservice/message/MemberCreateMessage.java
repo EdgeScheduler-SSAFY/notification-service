8ff033081cc609a17bf1c8daeb0c92f703db9c44
오형택 <oh052679@gmail.com>
2024-05-14 15:04:40 +0900

    🚑Hotfix :  fix field name
    Change-Id: I37136f9e6710de66b1c830f7e0207f80bfa3c3cd

--- src/main/java/com/edgescheduler/notificationservice/message/MemberCreateMessage.java
+++ src/main/java/com/edgescheduler/notificationservice/message/MemberCreateMessage.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class MemberCreateMessage {
 
-    private Integer id;
+    private Integer memberId;
     private String email;
     private String zoneId;
 }
